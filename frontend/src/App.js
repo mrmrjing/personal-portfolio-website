596d5fc17df49ace4a2cbd6df7dd703be1d7b8c5
mrmrjing <hongjing123321@gmail.com>
2025-01-08 16:28:08 +0800

    Added bootstrap css package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import ProjectsPage from './components/pages/ProjectsPage';
-import ContactPage from './components/pages/ContactPage';
 import StarsBackground from './components/common/StarsBackground';
 import Navbar from './components/layout/Navbar';
 import AboutPage from './components/pages/AboutPage';
@@ -10,6 +9,8 @@ import Footer from './components/layout/Footer';
 import ProjectDetailPage from './components/pages/ProjectDetailPage';
 import projectsData from './components/pages/ProjectsData';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
@@ -22,7 +23,6 @@ function App() {
                       <Route path="/" element={<HomePage />} />
                       <Route path="/projects" element={<ProjectsPage />} />
                       <Route path="/projects/:projectId" element={<ProjectDetailPage projectsData={projectsData} />} />
-                      <Route path="/contact" element={<ContactPage />} />
                       <Route path='/about' element={<AboutPage />} />
                   </Routes>
               </div>
