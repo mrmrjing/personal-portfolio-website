bfae37b090c4b2c43625cabb2afd04b2c262da6d
mrmrjing <hongjing123321@gmail.com>
2025-01-08 18:38:27 +0800

    Updated website content to fix project redirection

--- frontend/src/components/common/HeroSection.js
+++ frontend/src/components/common/HeroSection.js
@@ -5,7 +5,7 @@ const HeroSection = () => {
     <div className="text-center py-5 bg-dark text-white">
       <h1>I am an aspiring cybersecurity professional</h1>
       <p>Check out my latest projects below.</p>
-      <a href="personal-portfolio-website/projects" className="btn btn-primary mt-3">
+      <a href="/projects" className="btn btn-primary mt-3">
         View My Projects
       </a>
     </div>
