da90220d4f74c4365fb63a159438b1f4c9d53df8
mrmrjing <hongjing123321@gmail.com>
2025-01-08 18:46:32 +0800

    Use react router dom for navigation instead of href

--- frontend/src/components/common/HeroSection.js
+++ frontend/src/components/common/HeroSection.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
 
 const HeroSection = () => {
   return (
     <div className="text-center py-5 bg-dark text-white">
       <h1>I am an aspiring cybersecurity professional</h1>
       <p>Check out my latest projects below.</p>
-      <a href="/projects" className="btn btn-primary mt-3">
+      <Link to="/projects" className="btn btn-primary mt-3">
         View My Projects
-      </a>
+      </Link>
     </div>
   );
 };
